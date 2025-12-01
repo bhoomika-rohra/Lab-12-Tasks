df0dc857c88ac2a378e589b821be416500e207dd
bhoomika-rohra <K250784@nu.edu.pk>
2025-12-01 22:47:11 +0500

    Create task5.c

---
+++ task5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    char str1[100], str2[100];
+
+    printf("Enter first string: ");
+    fgets(str1, 100, stdin);
+    str1[strcspn(str1, "\n")] = '\0';
+
+    printf("Enter second string: ");
+    fgets(str2, 100, stdin);
+    str2[strcspn(str2, "\n")] = '\0'; 
+
+    int len1 = strlen(str1);
+    int len2 = strlen(str2);
+    char *concat = (char*)malloc(len1 + len2 + 1);
+
+    strcpy(concat, str1);
+    strcat(concat, str2);
+
+    printf("Concatenated string: %s\n", concat);
+
+    free(concat);
+    return 0;
+}
